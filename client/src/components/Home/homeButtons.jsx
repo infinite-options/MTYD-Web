f17c69d5ce20bde920a86fd8794e14a0b6bbabfa
Anup Jaltade <f16flier@gmail.com>
2021-05-01 03:03:44 -0500

    Favourite meals training card

--- client/src/components/Home/homeButtons.jsx
+++ client/src/components/Home/homeButtons.jsx
@@ -17,6 +17,7 @@ import signupButton from '../../images/Group 480.png';
 import negativeSign from '../../images/Group 504.png';
 import positiveSign from '../../images/Group 505.png';
 import continueExploring from '../../images/Group 575.png'
+import heartImage from '../../images/Icon ionic-ios-heart.png'
 
 class HomeLink extends Component {
     render() { 
@@ -553,4 +554,97 @@ class SkipMeals extends Component {
     )
   }
 }
-export {HomeLink, FootLink, AmbassadorLink, AddressLink, AddMeals, SaveMeals, SurpriseMeals, SkipMeals};
+class FavoriteMeal extends Component {
+
+  constructor(props){
+    super();
+    this.state = {
+      user_id:'',
+      user_address:'',
+      login_seen:false,
+      signUpSeen:false, 
+    };
+  }
+  handleClick = () => {
+    this.props.toggle();
+  };
+  togglePopLogin = () => {
+    this.setState({
+     login_seen: !this.state.login_seen,
+    });
+
+    if(!this.state.login_seen){
+      this.setState({
+        signUpSeen:false
+      })
+    }
+
+   };
+
+   togglePopSignup = () => {
+    this.setState({
+     signUpSeen: !this.state.signUpSeen
+    });
+
+    if(!this.state.signUpSeen){
+      this.setState({
+        login_seen:false
+      })
+    }
+   };
+
+  componentDidMount(){
+    const customer_uid = Cookies.get("customer_uid");
+    console.log(Cookies.get("customer_uid"))
+    if(customer_uid){
+      this.setState({user_id:customer_uid})
+      axios
+      .get(`${API_URL}Profile/${customer_uid}`)
+      .then((response) => {
+        const addr = response.data.result[0].customer_address.toLowerCase();
+        this.setState({user_address: addr});
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response);
+        }
+        console.log(err);
+      });
+      
+    }else{
+      this.setState({user_id:'not login'})
+      this.setState({user_address: 'not login yet'});
+	/*Use the following for setting the user */ 
+	 /*this.setState({user_id:'anup'});
+	this.setState({user_address: '214 Main Ave, San Jose, CA 91101'});*/
+    }
+    
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.login_seen ? <PopLogin toggle={this.togglePopLogin} /> : null}
+        {this.state.signUpSeen ? <Popsignup toggle={this.togglePopSignup} /> : null}
+
+        {(() => {
+			if(this.state.user_id == "not login") {
+					return (
+					  <div className={styles.modal}>
+						<div className={styles.modal_content}>
+			                <p className= {styles.ambassdorText} style = {{font:'SF Pro', fontSize: '24px', fontWeight:'medium', textAlign: 'left', color:'black'}}>Don’t forget your <span style={{color:'#F26522'}}>favorite</span> meals!<br/> Click the <img src ={heartImage}/> to easily find your favorite<br/> meals and get reminders.</p>	
+							<br/><br/>
+							<img style= {{display: 'block', marginLeft: '80px', marginRight: 'auto'}} src = {continueExploring}/>
+							<p style = {{marginLeft:'50px',font: 'SF Pro', fontWeight:'bold', fontSize:'18px',textAlign: 'left', paddingTop:'15px', color:'black'}}>Already a Customer?</p>
+							<img style= {{display: 'block', marginLeft: '80px',marginTop:'-20px', marginRight: 'auto'}} src = {loginButton}/>
+							<p style = {{marginLeft:'80px',font: 'SF Pro', fontWeight:'bold', fontSize:'18px',textAlign: 'left', paddingTop:'15px', color:'black'}}>Ready to start eating better?</p>
+							<img style= {{display: 'block', marginLeft: '80px', marginTop:'-15px',marginRight: 'auto'}} src = {signupButton}/>
+						</div>
+					  </div>
+				)}
+		}) ()}
+		</div>
+    )
+  }
+}
+export {HomeLink, FootLink, AmbassadorLink, AddressLink, AddMeals, SaveMeals, SurpriseMeals, SkipMeals, FavoriteMeal};
