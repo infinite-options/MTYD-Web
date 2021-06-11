cf37e5102c4c940a7526db32fdaa4e0db6f3d17f
Brandon Huss <bmhuss33@gmail.com>
2021-06-10 23:10:13 -0700

    Updates to admin customers page and admin nav bar designs

--- client/src/components/Admin/AdminNavBar/index.jsx
+++ client/src/components/Admin/AdminNavBar/index.jsx
@@ -1,7 +1,19 @@
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import whiteLogo from "../../../images/White_logo_for_web.png";
+import {connect} from "react-redux";
+import {withRouter} from "react-router-dom";
+import { useHistory } from "react-router";
+import styles from "./adminNavBar.module.css";
 
-function NavBar() {
-  return (
+function NavBar(props) {
+  // const history = useHistory();
+
+  // const goToLink = (navlink) => {
+  //   console.log("LINK CLICKED: ", navLink);
+  //   history.push(navlink);
+  // }
+
+  /*return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand href="/">Meal To Your Door</Navbar.Brand>
@@ -93,6 +105,47 @@ function NavBar() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+    </div>
+  );*/
+  return (
+    <div
+      style={{
+        border: 'solid',
+        backgroundColor: '#F8BB17',
+        display: 'flex',
+        alignItems: 'center',
+        height: '80px'
+      }}
+    >
+      <a 
+        href='/home' 
+        style={{
+          // margin:0,
+          // position:"absolute",
+          // width: "160px", 
+          // height:"80px",
+          // top:"5px",
+          border: 'inset',
+          backgroundImage:`url(${whiteLogo})`,
+          backgroundSize:'cover',
+          // backgroundPosition:'center',
+          // left:'48%',
+          width: '12%',
+          marginLeft: '1%',
+          marginRight: '1%',
+          minWidth: '120px',
+          height: '60px'
+        }}>
+      </a>
+      <a href='/admin/edit-meal' className={styles.navLink3}>Meals & Plans</a>
+      <a href='/admin/edit-meal' className={styles.navLink4}>Plans & Coupons</a>
+      <a href='/admin/edit-meal' className={styles.navLink5}>Orders & Ingredients</a>
+      <a href='/admin/edit-meal' className={styles.navLink2}>Customers</a>
+      <a href='/admin/edit-meal' className={styles.navLink4}>Google Analytics</a>
+      <a href='/admin/edit-meal' className={styles.navLink3}>Notifications</a>
+      <a href='/admin/edit-meal' className={styles.navLink2}>Businesses</a>
+      <a href='/admin/edit-meal' className={styles.navLink1}>Zones</a>
+      <a href='/admin/edit-meal' className={styles.navLink1}>Profile</a>
     </div>
   );
 }
--- client/src/components/Admin/CustomerInfo/index.js
+++ client/src/components/Admin/CustomerInfo/index.js
@@ -66,23 +66,63 @@ function CustomerInfo() {
 	}, []);
 
 	useEffect(() => {
-		console.log("dropdown menu set to: ", customerDropdown);
+		// console.log("dropdown menu set to: ", customerDropdown);
 	}, [customerDropdown]);
 
-	const showCustomerDropdown = (sortType) => {
+	const setCurrentCustomer = (cust) => {
+
+		console.log("set current customer: ", cust.customer_uid);
+
+		// Get additional customer info
+		/* AXIOS CALL HERE */
+		axios
+			.get(API_URL + 'predict_next_billing_date/' + cust.customer_uid)
+			.then(res => {
+				console.log("(pnba) next meal info res: ", res);
+
+				// let fetchedSubscriptions = res.data.result;
+
+				// this.displayErrorModal('Success!', `
+				// 	OLD MEAL PLAN: ${this.state.currentPlan.meals} meals, ${this.state.currentPlan.deliveries} deliveries
+				// 	NEW MEAL PLAN: ${this.state.updatedPlan.meals} meals, ${this.state.updatedPlan.deliveries} deliveries
+				// `, 
+				// 	'OK', 'back'
+				// );
+
+				// console.log("subscriptions loaded? ", this.state.subscriptionsLoaded);
+				// console.log(this.state.defaultSet === false);
+				// console.log(this.state.refreshingPrice);
+				// console.log(this.activeChanges());
+
+				// this.loadSubscriptions(fetchedSubscriptions, this.state.discounts, UPDATED);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+
+		selectCustomer(cust);
+	}
+
+	const showCustomerIDs = () => {
 		let sortedCustomerButtons = [];
 
 		customersById.forEach((cust) => {
 			// console.log("customer: ", cust);
 			sortedCustomerButtons.push(
 				<div
+				  key={cust.customer_uid}
 					style={{
 						border: 'solid',
 						borderColor: 'red',
 						borderWidth: '1px',
 						backgroundColor: 'yellow',
 						width: '120px',
-						zIndex: '5'
+						height: '30px',
+						zIndex: '3'
+					}}
+					onClick={() => {
+						console.log("clicked: ", cust.customer_uid);
+						setCurrentCustomer(cust);
 					}}
 				>
 					{cust.customer_uid}
@@ -92,9 +132,74 @@ function CustomerInfo() {
 
 		return (
 			<>
-				{/* <div>cust 1</div>
-				<div>cust 2</div>
-				<div>cust 3</div> */}
+				{sortedCustomerButtons}
+			</>
+		);
+	}
+
+	const showCustomerNames = () => {
+		let sortedCustomerButtons = [];
+
+		customersByName.forEach((cust) => {
+			// console.log("customer: ", cust);
+			sortedCustomerButtons.push(
+				<div
+				  key={cust.customer_uid}
+					style={{
+						border: 'solid',
+						borderColor: 'red',
+						borderWidth: '1px',
+						backgroundColor: 'yellow',
+						width: '120px',
+						height: '30px',
+						zIndex: '3'
+					}}
+					onClick={() => {
+						console.log("clicked: ", cust.customer_uid);
+						setCurrentCustomer(cust);
+					}}
+				>
+					{cust.customer_first_name}
+				</div>
+			);
+		});
+
+		return (
+			<>
+				{sortedCustomerButtons}
+			</>
+		);
+	}
+
+	const showCustomerEmails = () => {
+		let sortedCustomerButtons = [];
+
+		customersByEmail.forEach((cust) => {
+			// console.log("customer: ", cust);
+			sortedCustomerButtons.push(
+				<div
+				  key={cust.customer_uid}
+					style={{
+						border: 'solid',
+						borderColor: 'red',
+						borderWidth: '1px',
+						backgroundColor: 'yellow',
+						width: '240px',
+						height: '30px',
+						zIndex: '3'
+					}}
+					onClick={() => {
+						console.log("clicked: ", cust.customer_uid);
+						setCurrentCustomer(cust);
+					}}
+				>
+					{cust.customer_email}
+				</div>
+			);
+		});
+
+		return (
+			<>
 				{sortedCustomerButtons}
 			</>
 		);
@@ -149,6 +254,8 @@ function CustomerInfo() {
 							height: '30px',
 							marginLeft: '2%',
 							marginRight: '2%',
+							zIndex: '2',
+							cursor: 'pointer'
 							// position: 'relative',
 							// display: 'flex',
 							// alignItems: 'flex-start'
@@ -159,7 +266,7 @@ function CustomerInfo() {
 								: setCustomerDropdown(CUS_SEL_NONE)
 						}}
 					>
-						{showCustomerDropdown(CUS_SEL_ID)}
+						{showCustomerIDs()}
 					</div>
 				) : (
 					<div
@@ -169,7 +276,8 @@ function CustomerInfo() {
 							borderWidth: '1px',
 							width: '16%',
 							marginLeft: '2%',
-							marginRight: '2%'
+							marginRight: '2%',
+							cursor: 'pointer'
 						}}
 						onClick={() => {
 							customerDropdown === CUS_SEL_NONE
@@ -181,41 +289,193 @@ function CustomerInfo() {
 					</div>
 				)}
 
-				<div
-					type='text'
-					style={{
-						border: 'solid',
-						borderWidth: '1px',
-						width: '16%',
-						marginLeft: '2%',
-						marginRight: '2%'
-					}}
-					onClick={() => {
-						customerDropdown === CUS_SEL_NONE
-							? setCustomerDropdown(CUS_SEL_NAME)
-							: setCustomerDropdown(CUS_SEL_NONE)
-					}}
-				>
-					Name
-				</div>
-				<div
-					type='text'
-					style={{
-						border: 'solid',
-						borderWidth: '1px',
-						width: '16%',
-						marginLeft: '2%',
-						marginRight: '2%'
-					}}
-					onClick={() => {
-						customerDropdown === CUS_SEL_NONE
-							? setCustomerDropdown(CUS_SEL_EMAIL)
-							: setCustomerDropdown(CUS_SEL_NONE)
-					}}
-				>
-					Email
-				</div>
+				{customerDropdown === CUS_SEL_NAME ? (
+					<div
+						type='text'
+						style={{
+							border: 'solid',
+							borderWidth: '1px',
+							width: '16%',
+							height: '30px',
+							marginLeft: '2%',
+							marginRight: '2%',
+							zIndex: '2',
+							cursor: 'pointer'
+							// position: 'relative',
+							// display: 'flex',
+							// alignItems: 'flex-start'
+						}}
+						onClick={() => {
+							customerDropdown === CUS_SEL_NONE
+								? setCustomerDropdown(CUS_SEL_NAME)
+								: setCustomerDropdown(CUS_SEL_NONE)
+						}}
+					>
+						{showCustomerNames()}
+					</div>
+				) : (
+					<div
+						type='text'
+						style={{
+							border: 'solid',
+							borderWidth: '1px',
+							width: '16%',
+							marginLeft: '2%',
+							marginRight: '2%',
+							cursor: 'pointer'
+						}}
+						onClick={() => {
+							customerDropdown === CUS_SEL_NONE
+								? setCustomerDropdown(CUS_SEL_NAME)
+								: setCustomerDropdown(CUS_SEL_NONE)
+						}}
+					>
+						Name
+					</div>
+				)}
+
+				{customerDropdown === CUS_SEL_EMAIL ? (
+					<div
+						type='text'
+						style={{
+							border: 'solid',
+							borderWidth: '1px',
+							width: '16%',
+							height: '30px',
+							marginLeft: '2%',
+							marginRight: '2%',
+							zIndex: '2',
+							cursor: 'pointer'
+							// position: 'relative',
+							// display: 'flex',
+							// alignItems: 'flex-start'
+						}}
+						onClick={() => {
+							customerDropdown === CUS_SEL_NONE
+								? setCustomerDropdown(CUS_SEL_EMAIL)
+								: setCustomerDropdown(CUS_SEL_NONE)
+						}}
+					>
+						{showCustomerEmails()}
+					</div>
+				) : (
+					<div
+						type='text'
+						style={{
+							border: 'solid',
+							borderWidth: '1px',
+							width: '16%',
+							marginLeft: '2%',
+							marginRight: '2%',
+							cursor: 'pointer'
+						}}
+						onClick={() => {
+							customerDropdown === CUS_SEL_NONE
+								? setCustomerDropdown(CUS_SEL_EMAIL)
+								: setCustomerDropdown(CUS_SEL_NONE)
+						}}
+					>
+						Email
+					</div>
+				)}
+
 			</div>
+
+			{/* {selectedCustomer === null ? (
+				<div
+					style={{
+						border: 'solid',
+						marginTop: '20px',
+						height: '50px',
+						zIndex: '1'
+					}}
+				>
+					Waiting for selection...
+				</div>
+			) : (
+				<div
+					style={{
+						border: 'solid',
+						marginTop: '20px',
+						height: '50px',
+						zIndex: '1'
+					}}
+				>
+					Current Customer: {selectedCustomer.customer_uid}
+				</div>
+			)} */}
+			{/* {selectedCustomer === null ? (
+				<div
+					style={{
+						border: 'solid',
+						marginTop: '20px',
+						height: '50px',
+						zIndex: '1'
+					}}
+				>
+					Waiting for selection...
+				</div>
+			) : ( */}
+
+			{console.log("Current customer info: ", selectedCustomer)}
+			<div
+				style={{
+					border: 'solid',
+					marginTop: '20px',
+					height: '300px',
+					zIndex: '1'
+				}}
+			>
+				{selectedCustomer === null ? (
+					<>{"Waiting for selection..."}</>
+				) : (
+					<>
+						{"Current Customer: " + selectedCustomer.customer_uid}
+						<br />
+						<br />
+						{
+							selectedCustomer.customer_first_name + " " + 
+							selectedCustomer.customer_last_name
+						}
+						<br />
+						<br />
+						{"Delivery Info: "}
+						<br />
+						{selectedCustomer.customer_address}
+						<br />
+						{selectedCustomer.customer_city}
+						<br />
+						{selectedCustomer.customer_zip}
+						<br />
+						<br />
+						{"Contact Info: "}
+						<br />
+						{selectedCustomer.customer_email}
+						<br />
+						{selectedCustomer.customer_phone}
+					</>
+				)}
+			</div>
+
+			{/* Meal Plans */}
+			<div
+				style={{
+					border: 'solid',
+					marginTop: '20px',
+					height: '300px',
+					zIndex: '1'
+				}}
+			>
+				{selectedCustomer === null ? (
+					<>{"Waiting for selection..."}</>
+				) : (
+					<>
+						{"Meal Plans: "}
+						<br />
+					</>
+				)}
+			</div>
+
 		</>
   )
 }
